507ca279e1405ff6d9883a309ccfe82040cf06cf
mrunlockedtech <jrbhrn@gmail.com>
2022-09-01 19:48:18 -0500

    Add initial state variables for board, turn, and winner

--- js/app.js
+++ js/app.js
@@ -3,7 +3,11 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
+//Create variables to track state of game
+// board - state of squares, turn is tracking the turn of player, and winner if 
+// a player has won or tied
 
+let board, turn, winner
 
 
 /*------------------------ Cached Element References ------------------------*/
